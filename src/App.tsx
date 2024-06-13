83019be1bf0ca8b224d0018b906d04bdd480f89e
Ilahazs <ilahazs@computer>
2024-06-13 11:03:50 +0700

    chore: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Login from "./components/pages/Login";
+import Login2 from "./components/pages/Login2";
+
+export default function App() {
+    return (
+        <main className="h-screen flex items-center justify-center">
+            {/* <Login /> */}
+            <Login2 />
+        </main>
+    );
+}
